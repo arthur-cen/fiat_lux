eeadd428ed3f7debf52d0da8aef53b96ae9e0296
susan-xu <Susan@xus-macbook.wifi.cmu.edu>
2020-02-01 14:57:52 -0500

    add simple start scene

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -1,8 +1,8 @@
-import 'phaser'
+import 'phaser';
 
 export default class StartScene extends Phaser.Scene {
     constructor() {
-        super('Game')
+        super('Start');
     }
 
     preload() {
@@ -10,6 +10,13 @@ export default class StartScene extends Phaser.Scene {
     }
 
     create() {
+
+        this.add.text(250, 100, 'Flat_lux', {font: "78px Arial", fill: "white"});
+        this.add.text(250, 200, 'A journey on light.', {font: "36px Arial", fill: "yellow"});
+
+        const clickButton = this.add.text(300, 300, 'Start the journey.', { fill: 'white' })
+        .setInteractive()
+        .on('pointerdown', () => this.scene.start("Game") );
 
     }
 
