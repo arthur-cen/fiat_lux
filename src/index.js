e207687db1da77cdd7f6dd027f3275805df3047f
Shengrui Cen <cenrui.93@gmail.com>
2020-02-01 14:14:00 -0500

    change the initial scene to start scene

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Phaser from "phaser";
 import config from "./config/config";
 import GameScene from "./scenes/GameScene";
+import StartScene from "./scenes/StartScene";
 class Game extends Phaser.Game {
   constructor() {
     super(config);
     this.scene.add('Game', GameScene);
-    this.scene.start('Game');
+    this.scene.add('Start', StartScene);
+    this.scene.start('Start');
   }
 }
 
