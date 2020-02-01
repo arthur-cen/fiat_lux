8fdfbf408995f1c7dae49e9ae2479c2a1b33080f
susan-xu <Susan@xus-macbook.wifi.cmu.edu>
2020-02-01 15:12:56 -0500

    add more interactive scene

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -14,11 +14,25 @@ export default class StartScene extends Phaser.Scene {
         this.add.text(250, 100, 'Flat_lux', {font: "78px Arial", fill: "white"});
         this.add.text(250, 200, 'A journey on light.', {font: "36px Arial", fill: "yellow"});
 
-        const clickButton = this.add.text(300, 300, 'Start the journey.', { fill: 'white' })
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start("Game") );
+        this.clickButton = this.add.text(300, 300, 'Start the journey', { fill: 'white' })
+        .setInteractive({ useHandCursor: true })
+        .on('pointerdown', () => this.scene.start("Game") )
+        .on('pointerover', () => this.enterButtonHoverState() )
+        .on('pointerout', () => this.enterButtonRestState() )
+        .on('pointerdown', () => this.enterButtonActiveState() );
 
     }
+    enterButtonHoverState() {
+        this.clickButton.setStyle({ fill: 'yellow'});
+    }
+
+    enterButtonRestState() {
+        this.clickButton.setStyle({ fill: 'white' });
+    }
+
+    enterButtonActiveState() {
+        this.clickButton.setStyle({ fill: 'red' });
+      }
 
     update() {
 
